18b4089c60c560cd379a78d7201c5514030fdbd7
Andrea aus HH <andrealazzaretti@gmail.com>
2024-02-07 23:56:52 +0100

    Remove yarn-based build script

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-REV=$(($(date +%s%N)/1000000))
-ZIP_BUILD="build"
-
-rm ./*.zip
-yarn build
-rm ./public/build/*.map
-cd ./public && zip -r "./${ZIP_BUILD}_${REV}.zip" * && cp "./${ZIP_BUILD}_${REV}.zip" ../ && rm "./${ZIP_BUILD}_${REV}.zip"
